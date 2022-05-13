84f95f044c17c7a08517edf0e747f631265155ab
Muhammad-yogi <yogikgaek@gmail.com>
2022-05-13 13:02:39 +0700

    /

--- app.js
+++ app.js
@@ -17,6 +17,5 @@ app.use(`/`, (req, res) => {
     res.send(`<h1> Page not found </h1>`);
 });
 
-const PORT = process.env.PORT || 4000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
